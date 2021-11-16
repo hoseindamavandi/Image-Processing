8032f207081e80587c7430fefec1f6855b948900
Hosein Damavandi <83751182+hoseindamavandi@users.noreply.github.com>
2021-11-17 00:52:27 +0330

    Delete 22 directory

--- 22/4/4.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('chess pieces.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-print(img.shape)
-for i in range(7):
-    a = img[0:275,i*83:83+i*83]
-    cv2.imwrite(f'chesspieces{i}.jpg',a)
