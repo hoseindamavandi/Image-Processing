57ca016373ec9a91e6507468c694da702722fce4
hoseindamavandi <h.damavandi2012@yahoo.com>
2021-11-17 10:58:09 +0330

    update level1

--- Level-1/be happy(rotate)/3.py
+++ Level-1/be happy(rotate)/3.py
@@ -1,11 +1,9 @@
 import cv2
 
-if __name__ == '__main__' :
-    image1=cv2.imread('3.jpg')
+image1 = cv2.imread('/Users/hossein/Documents/python/Image-Processing/Level-1/be happy(rotate)/3.jpg')
 
-    image1=cv2.rotate(image1, cv2.ROTATE_180)
+image1 = cv2.rotate(image1, cv2.ROTATE_180)
 
-    cv2.imwrite('C:/Users/Hosein/Desktop/image3.jpg', image1)
-
-    cv2.imshow('show output',image1)
-    cv2.waitKey()
+cv2.imshow('show output',image1)
+cv2.waitKey()
+cv2.imwrite('output3.jpg', image1)
